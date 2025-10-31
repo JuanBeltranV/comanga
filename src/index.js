24500e15d8362a2dd6a54671cbc21be242ec858d
JuanBeltranV <jua.beltranv@duocuc.cl>
2025-10-31 03:21:59 -0300

    Añadir basename a BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './styles.css';                          // Tu CSS global (copialo aquí)
+import './styles.css';                          // Tu CSS global (copialo aquí)
 import App from './App.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/comanga">
     <App />
   </BrowserRouter>
 );
