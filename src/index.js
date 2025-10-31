f87b7f6e1adbebb8ef363be83d6443be310136e5
JuanBeltranV <jua.beltranv@duocuc.cl>
2025-10-31 03:28:30 -0300

    Cambiar a HashRouter para compatibilidad con GH Pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 // src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// 1. CAMBIA ESTA LÍNEA:
+import { HashRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './styles.css';                          // Tu CSS global (copialo aquí)
+import './styles.css';
 import App from './App.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/comanga">
+  // 2. CAMBIA ESTO (y quita el basename):
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
